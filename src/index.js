42c86340cb92514dd87c37a1c2a6d80051ecbbb2
JuGeonjeong <81910342+JuGeonjeong@users.noreply.github.com>
2022-02-06 18:44:14 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import AdminLayout from "layouts/Admin.js";
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-          <Route path="/reactNewbie" render={(props) => <AdminLayout {...props} />} />
+          <Route path="/reactNewbie"  render={(props) => <AdminLayout {...props} />}/>
         </Switch>
       </BrowserRouter>,
   document.getElementById('root')
@@ -16,4 +16,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
